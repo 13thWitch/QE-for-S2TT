f8ff64708cbc170527ce27b377abaa7e5f396680
Iva Andreeva <iva.andreeva@student.kit.edu>
2024-05-21 11:27:44 +0200

    ADD: tested audio splitting and SpecAugment result conversion to audio

--- augment_experiments.py
+++ augment_experiments.py
@@ -1,8 +1,10 @@
 from datasets import load_dataset
 from augment_notfa import SpecAugment
 import librosa
+import soundfile as sf
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 import os
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +15,9 @@ ds = load_dataset("covost2", "pt_en", data_dir="./datasets/pt", split="test", tr
 audio_sample = ds[4]["audio"]
 waveform = audio_sample["array"]
 sampling_rate = audio_sample["sampling_rate"]
+
+# Save the audio file
+sf.write('unperturbed_audio_file.wav', waveform, sampling_rate)
 
 # Extract Mel Spectrogram Features from the audio file
 mel_spectrogram = librosa.feature.melspectrogram(y=waveform, sr=sampling_rate, n_mels=256, hop_length=128, fmax=8000)
@@ -25,6 +30,13 @@ apply = SpecAugment(mel_spectrogram, policy='LB')
 
 # mask frequency
 freq_masked = apply.freq_mask()
+
+# convert back to audio signal
+# This is unavoidably lossy and introduces some noise/metallic sound. Minimal at about 2048 n_fft and 128 hop_length
+perturbed_audio = librosa.feature.inverse.mel_to_audio(freq_masked, sr=sampling_rate, n_fft=2048, hop_length=128)
+# sf.write('perturbed_audio.wav', perturbed_audio[:math.floor(len(perturbed_audio)/3)], sampling_rate)
+sf.write('perturbed_audio.wav', perturbed_audio, sampling_rate)
+
 plt.figure(figsize=(14, 6))
 # librosa.display.specshow(librosa.power_to_db(freq_masked, ref=np.max), x_axis='time', y_axis='mel', fmax=8000)
 librosa.display.specshow(librosa.power_to_db(freq_masked[:,:], ref=np.max), x_axis='time', y_axis='mel', fmax=8000)
